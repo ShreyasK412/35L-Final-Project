c16e3371235ea0581277688aecc69af6dbbbb258
joeshiller <125774851+joeshiller@users.noreply.github.com>
2023-03-06 17:30:16 -0800

    Completed Nav Bar
    Added tweet embedding on the News Page

--- frontend/src/Pages/news.js
+++ frontend/src/Pages/news.js
@@ -1,3 +1,18 @@
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
 export default function News() {
-    return <h1>News</h1>
+    return (
+    <div>
+        <TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="ESPN"
+  options={{height: 800, width: 500}}
+/>
+
+        {/* <TwitterTweetEmbed
+  tweetId={'1631080648999075841'}
+/> */}
+      
+    </div>
+    )
 }
