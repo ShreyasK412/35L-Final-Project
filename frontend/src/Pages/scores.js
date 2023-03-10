e80939986c45d0d763e1ab4f71de3bfb0c3baca6
Antonio Cosio <coshio@g.ucla.edu>
2023-03-09 22:52:09 -0800

    Implemented a PL Standings using API-FOOTBALL to Scores tab
    Fetches the real-time data using api found at "https://v3.football.api-sports.io/
    Updates standings automatically using dynamic replacement

--- frontend/src/Pages/scores.js
+++ frontend/src/Pages/scores.js
@@ -1,18 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import debounce from 'lodash/debounce';
 
+import './scores.css';
 
+export default function Scores() {
+    const FOOTBALL_KEY = "25e3dcc42b7508519df698b88599a569";
+    const url = "https://v3.football.api-sports.io/standings?league=39&season=2022";
+    fetch(url, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "v3.football.api-sports.io",
+            "x-rapidapi-key": FOOTBALL_KEY,
+        }
+    })
 
-// added debounce to try to reduce requests
-// imported Axios to get the API data
-// Possible fix: useMemo(() => 
+    .then(response => response.json())
+    .then(data => {
 
+        const standingsData = data.response[0].league.standings[0];
+        const sortedData = standingsData.sort((a, b) => a.rank - b.rank);
 
-function Scores() {
+        const tbody = document.querySelector('#standings-table tbody');
+        tbody.innerHTML = ``;
+        
+        sortedData.forEach((team, index) => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = ``;
+            tr.innerHTML = `
+            <td>
+                <span class="rank">${team.rank}</span>
+                <img src=${team.team.logo} class="logos">
+                <span class="team-name">${team.team.name}</span>
+            </td>
+            <td>${team.all.played}</td>
+            <td>${team.all.win}</td>
+            <td>${team.all.draw}</td>
+            <td>${team.all.lose}</td>
+            <td>${team.all.goals.for}</td>
+            <td>${team.all.goals.against}</td>
+            <td>${team.goalsDiff}</td>
+            <td>${team.points}</td>
+            `;
+            tbody.appendChild(tr);
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
+    return (
+        <div>
+        <h1 class="premleague">Premier League Standings <img class="premleagueicon" src="https://media.api-sports.io/football/leagues/39.png" alt=""></img></h1>
+        <table id="standings-table">
+            <thead>
+                <tr>
+                <th>Club</th>
+                <th>MP</th>
+                <th>W</th>
+                <th>D</th>
+                <th>L</th>
+                <th>GF</th>
+                <th>GA</th>
+                <th>GD</th>
+                <th>Pts</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+        </div>
+    )
 }
-
-
-export default Scores;
-
