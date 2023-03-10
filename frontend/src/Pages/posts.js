3b05920ed44470c29d2090bd1164d8c2a51343b5
Shreyas Kamath <shreyaskamath203@gmail.com>
2023-03-09 18:25:29 -0800

    Fixed some of the styling issues

--- frontend/src/Pages/posts.js
+++ frontend/src/Pages/posts.js
@@ -2,6 +2,35 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../Components/Navbar';
 import './posts.css';
 
+function LikeButton({ postId, likes, updateLikes }) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikeClick = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/api/posts/${postId}/like`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ isLiked: !isLiked }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update like.');
+      }
+
+      setIsLiked(!isLiked);
+      updateLikes(isLiked ? likes - 1 : likes + 1);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <button className="like-button" onClick={handleLikeClick}>
+      {isLiked ? 'Unlike' : 'Like'} ({likes})
+    </button>
+  );
+}
+
 export default function Posts() {
   const [posts, setPosts] = useState([]);
   const [title, setTitle] = useState('');
@@ -43,6 +72,14 @@ export default function Posts() {
     }
   };
 
+  const handleLikesUpdate = (postId, newLikes) => {
+    setPosts((prevPosts) =>
+      prevPosts.map((post) =>
+        post._id === postId ? { ...post, likes: newLikes } : post
+      )
+    );
+  };
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -64,52 +101,54 @@ export default function Posts() {
   }, []);
 
   return (
-    <div className='post'>
-     
-      <div align="center" className="create-post">
-        <h1>Create a new post</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={handleTitleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="content">Content:</label>
-            <textarea
-              id="content"
-              value={content}
-              onChange={handleContentChange}
-            />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-        {error && <p>{error}</p>}
-      </div>
-      
-      <div align="center" className="recent-posts" style={{position: "relative"}}>
-        <h1>Recent Posts</h1>
-        <div>
-          {posts.map((post) => (
-            <div key={post._id} className="post-box">
-              <div key={(post.tile)} className = "post-title">
-                <h2>{post.title}</h2>
-              </div>
-              <div key={(post.content)} className = "post-content">
-                <p>{post.content}</p>
-              </div>
-              <div className="post-meta">
-                <span className="post-likes">Likes: {post.likes}</span>
-                <span className="post-timestamp">Creat:{new Date(post.createdAt).toLocaleString()}</span>
-              </div>
-            </div>
-          ))}
-          {error && <p>{error}</p>}
-        </div>
-      </div>
+    <div align="center" className="create-post">
+  <h1>Create a new post</h1>
+  <form onSubmit={handleSubmit}>
+    <div>
+      <label htmlFor="title">Title:</label>
+      <input
+        type="text"
+        id="title"
+        value={title}
+        onChange={handleTitleChange}
+      />
     </div>
-  );}
+    <div>
+      <label htmlFor="content">Content:</label>
+      <textarea
+        id="content"
+        value={content}
+        onChange={handleContentChange}
+      />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+  {error && <p>{error}</p>}
+
+
+    <div className="post-container">
+      {/* ... */}
+      {posts.map((post) => (
+        <div className="post-box" key={post._id}>
+          <div className="post">
+            <div className="post-header">
+              <h2 className="post-title" style={{ textAlign: "center" }}>{post.title} </h2>
+              <p className="post-timestamp">{post.createdAt}</p>
+            </div>
+            <p className="post-content">{post.content}</p>
+            <div className="post-footer">
+              <LikeButton
+                postId={post._id}
+                likes={post.likes}
+                updateLikes={handleLikesUpdate}
+              />
+              <p className="post-likes">{post.likes} Likes</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+    </div>
+  );
+}
+  
