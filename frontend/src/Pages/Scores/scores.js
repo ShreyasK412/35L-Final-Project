2f9c1342f5821fdb0054732f0c294fd0824304aa
joeshiller <125774851+joeshiller@users.noreply.github.com>
2023-03-15 01:05:17 -0700

    Updated scores styling

--- frontend/src/Pages/Scores/scores.js
+++ frontend/src/Pages/Scores/scores.js
@@ -33,7 +33,11 @@ function Scores () {
      <div> 
      <h1><img className="premleague-title-icon" src="https://media.api-sports.io/football/leagues/39.png" alt=""></img></h1>
      </div>
+     <div> 
+     <h1><img className="bundesliga-title-icon" src="https://media.api-sports.io/football/leagues/78.png" alt=""></img></h1>
     
+     </div>
+     <h1><img className="laliga-title-icon" src="https://media.api-sports.io/football/leagues/140.png" alt=""></img></h1>
      </div>
      
      </>); 
