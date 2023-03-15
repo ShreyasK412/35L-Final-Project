b1bd06bd5082d5c5d02347d4dd017364835fcc78
Antonio Cosio <coshio@g.ucla.edu>
2023-03-14 22:08:12 -0700

    Fixed issue with soccer page
    Merging conflicts led to incorrect files

--- frontend/src/Pages/Scores/Soccer/soccer.js
+++ frontend/src/Pages/Scores/Soccer/soccer.js
@@ -1,7 +1,18 @@
 import PremLeague from './premleague'
+import LaLiga from './laliga'
+import Bundesliga from './bundesliga'
 
-export default function Soccer () {
+export default function Soccer() {
     return (
-    <div> <PremLeague/> </div>
+        <div>
+        <h1><img className="premleague-title-icon" src="https://media.api-sports.io/football/leagues/39.png" alt=""></img></h1>
+        <PremLeague/>
+        <br/>
+        <h1><img className="laliga-title-icon" src="https://media.api-sports.io/football/leagues/140.png" alt=""></img></h1>
+        <LaLiga/>
+        <br/>
+        <h1><img className="bundesliga-title-icon" src="https://media.api-sports.io/football/leagues/78.png" alt=""></img></h1>
+        <Bundesliga/>
+        </div>
     )
 }
