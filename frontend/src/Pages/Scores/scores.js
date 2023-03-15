d14f6da2ded5fa7c64ba7f595af7bfd0f9d56944
joeshiller <125774851+joeshiller@users.noreply.github.com>
2023-03-15 01:13:42 -0700

    Scores.js logos added

--- frontend/src/Pages/Scores/scores.js
+++ frontend/src/Pages/Scores/scores.js
@@ -27,18 +27,21 @@ function Scores () {
      <ScoreNavbar/>
      <div className='container'> {component}
     
-     <h1> <img className="nba-title-icon" src="https://1000logos.net/wp-content/uploads/2017/04/Logo-NBA.png" alt=""></img>
-     </h1>
-     
-     <div> 
-     <h1><img className="premleague-title-icon" src="https://media.api-sports.io/football/leagues/39.png" alt=""></img></h1>
-     </div>
-     <div> 
-     <h1><img className="bundesliga-title-icon" src="https://media.api-sports.io/football/leagues/78.png" alt=""></img></h1>
-    
-     </div>
-     <h1><img className="laliga-title-icon" src="https://media.api-sports.io/football/leagues/140.png" alt=""></img></h1>
-     </div>
+     <h1> 
+         <img className="nba-title-icon" src="https://1000logos.net/wp-content/uploads/2017/04/Logo-NBA.png" alt=""/>
+      </h1>
+      <div style={{ display: "flex" }}>
+         <h1>
+            <img className="premleague-title-icon" src="https://media.api-sports.io/football/leagues/39.png" alt=""/>
+         </h1>
+         <h1>
+            <img className="bundesliga-title-icon" src="https://media.api-sports.io/football/leagues/78.png" alt=""/>
+         </h1>
+         <h1>
+            <img className="laliga-title-icon" src="https://media.api-sports.io/football/leagues/140.png" alt=""/>
+         </h1>
+      </div>
+   </div>
      
      </>); 
 }
