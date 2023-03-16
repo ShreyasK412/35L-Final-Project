ffcd28875e885dfc256cc5e65507ab37659ab9a7
frankson2390 <gabewiz@icloud.com>
2023-03-16 15:29:33 -0700

    Finished up home page implementation
    Cleaned up design issues and bugs as well as added additional video features to home page

--- frontend/src/Pages/home.js
+++ frontend/src/Pages/home.js
@@ -5,9 +5,31 @@ export default function Home() {
         <h1 class = "title">sports central</h1>
             <div align="center">
                 <iframe width="966" height="543" src="https://www.youtube.com/embed/2S6PGsZpQqk" ></iframe>
-                
+            </div>
+            <div class="youtubeplayer" align = "center">
+                <iframe width="966" height="543" src="https://www.youtube.com/embed/GAphpvxsgWA" ></iframe>
+            </div>
+            <div class="youtubeplayer" align = "center">
+                <iframe width="966" height="543" src="https://www.youtube.com/embed/Pyo-wJ-Pzx4" ></iframe>
+            </div>
+            <div class="topictext">
+                <p>We've worked extensively on a new webapp that combines all the text you'll need into one area.</p>
+            </div>
+            <div class="paragraphtext">
+                <p>This new app is the new way to interact with all your favorite online sports content, from sports scores, upcoming matches, tweet history from your favorite athletes and organizations, and also a discussion thread to talk about the lastest happenings with your favorite teams and sports.</p>
+                <p>You won't find anything like this anywhere else.</p>
             </div>
         </div>
+        <footer>
+            <br></br>
+            <div>
+            <a href="https://github.com/ShreyasK412/35L-Final-Project"><img class="footerimages" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="auto" height="50px"/></a>
+            <a href="https://www.reddit.com/r/ucla/"><img class="footerimages" src="https://cdn-icons-png.flaticon.com/512/52/52053.png" width="auto" height="50px"/></a>
+            <a href="https://twitter.com/ucla"><img class="footerimages" src="https://cdn-icons-png.flaticon.com/512/60/60580.png" width="auto" height="50px"/></a>
+            <a href="https://www.instagram.com/ucla"><img class="footerimages" src="https://www.pngall.com/wp-content/uploads/5/Instagram-Logo.png" width="auto" height="50px"/></a>
+            </div>
+        </footer>
+        
 
         </>
     )
