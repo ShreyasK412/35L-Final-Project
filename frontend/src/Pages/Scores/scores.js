320b65cba2e46448e717e63f05a559d3ab8f3898
frankson2390 <gabewiz@icloud.com>
2023-03-16 16:48:28 -0700

    Image Hyperlinks
    Made images divert to their correct score/stats pages on user click as well as fixed icon formatting on the score page

--- frontend/src/Pages/Scores/scores.js
+++ frontend/src/Pages/Scores/scores.js
@@ -22,26 +22,39 @@ function Scores () {
        break
  
     }
+
+
+    
      return (<>
      <ScoreNavbar/>
-     <div className='container'> {component}
+     <div className='league_icons'> {component}
     
      <h1> 
+      <a href = '/basketball'>
          <img className="nba-title-icon" src="https://1000logos.net/wp-content/uploads/2017/04/Logo-NBA.png" alt=""/>
+      </a>
       </h1>
       <div style={{ display: "flex" }}>
          <h1>
+         <a href = '/soccer'>
             <img className="premleague-title-icon" src="https://media.api-sports.io/football/leagues/39.png" alt=""/>
+         </a>
          </h1>
          <h1>
+         <a href = '/soccer'>
             <img className="laliga-title-icon" src="https://media.api-sports.io/football/leagues/140.png" alt=""/>
+            </a>
          </h1>
          <h1>
+         <a href = '/soccer'>
             <img className="bundesliga-title-icon" src="https://media.api-sports.io/football/leagues/78.png" alt=""/>
+            </a>
          </h1>
       </div>
       <h1> 
+      <a href = '/football'>
          <img className="nfl-title-icon" src="https://cdn.cookielaw.org/logos/46acd508-0e8d-40cd-af22-1a8bdfa6da60/e9c29623-f807-422e-9944-964ce7fff1e0/a67792a1-43d4-44d0-8d5e-99ce69b835d9/National_Football_League_logo.svg.png" alt=""></img>
+      </a>
       </h1>
    </div>
      
