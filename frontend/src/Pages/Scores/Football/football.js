4ca303671de3d63ed78c5c0b880400b27341f0b8
Antonio Cosio <coshio@g.ucla.edu>
2023-03-15 20:18:41 -0700

    Styled both NFL Conference Standings
    Fixed issue with wrapping text in home,road, and conf

--- frontend/src/Pages/Scores/Football/football.js
+++ frontend/src/Pages/Scores/Football/football.js
@@ -4,7 +4,7 @@ import FootballStandings from "./nfl-standings";
 export default function Football() {
     return (
         <div>
-        <h1><img className="nfl-title-icon" alt=""></img></h1>
+        <h1><img className="nfl-title-icon" src="https://cdn.cookielaw.org/logos/46acd508-0e8d-40cd-af22-1a8bdfa6da60/e9c29623-f807-422e-9944-964ce7fff1e0/a67792a1-43d4-44d0-8d5e-99ce69b835d9/National_Football_League_logo.svg.png" alt=""></img></h1>
             <div> <FootballStandings/> </div>
         </div>
     )
--- frontend/src/Pages/Scores/Football/nfl-standings.js
+++ frontend/src/Pages/Scores/Football/nfl-standings.js
@@ -21,6 +21,7 @@ export default function FootballStandings() {
                 sortedData[i].team.logo = "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/New_York_Jets_logo.svg/1200px-New_York_Jets_logo.svg.png";
             }
         }
+        // console.log(sortedData);
 
         const tbody = document.querySelector('#standings-table-nfl-american tbody');
         // console.log(tbody);
@@ -33,19 +34,66 @@ export default function FootballStandings() {
             <td>
                 <span class="rank">${index + 1}</span>
                 <img src=${team.team.logo} class="logos">
-                <span class="team-name">${team.team.name}</span>
+                <span class="team-name">${team.team.name.split(' ').pop()}</span>
             </td>
             <td>${team.won}</td>
             <td>${team.lost}</td>
             <td>${(team.won / (team.won + team.lost)).toFixed(3)}</td>
             <td>${team.points.for}</td>
             <td>${team.points.against}</td>
-            <td>${team.difference}</td>
-            <td>${team.records.home}${team.ties === 0 ? '-0' : ''}</td>
-            <td>${team.records.road}${team.ties === 0 ? '-0' : ''}</td>
-            <td>${team.records.division}${team.ties === 0 ? '-0' : ''}</td>
-            <td>${team.records.conference}${team.ties === 0 ? '-0' : ''}</td>
-            <td>${team.records.streak}</td>
+            <td>${team.points.difference}</td>
+            <td>${team.records.home}${team.records.home.split('-').length === 2 ? '-0' : ''}</td>
+            <td>${team.records.road}${team.records.road.split('-').length === 2 ? '-0' : ''}</td>
+            <td>${team.records.division}${team.records.division.split('-').length === 2 ? '-0' : ''}</td>
+            <td>${team.records.conference}${team.records.conference.split('-').length === 2 ? '-0' : ''}</td>
+            <td>${team.streak}</td>
+            `;
+            tbody.appendChild(tr);
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+    const url_national = "https://v1.american-football.api-sports.io/standings?league=1&season=2022&conference=National Football Conference";
+    fetch(url_national, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "v1.american-football.api-sports.io",
+            "x-rapidapi-key": FOOTBALL_KEY,
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const standingsData = data.response;
+        // console.log(standingsData);
+        const sortedData = standingsData.sort((a,b) => ((a.won / (a.won + a.lost)) - (b.won / (a.won + a.lost)))).reverse();
+        // console.log(sortedData);
+
+        const tbody = document.querySelector('#standings-table-nfl-national tbody');
+        // console.log(tbody);
+        tbody.innerHTML = ``;
+        
+        sortedData.forEach((team, index) => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = ``;
+            tr.innerHTML = `
+            <td>
+                <span class="rank">${index + 1}</span>
+                <img src=${team.team.logo} class="logos">
+                <span class="team-name">${team.team.name.split(' ').pop()}</span>
+            </td>
+            <td>${team.won}</td>
+            <td>${team.lost}</td>
+            <td>${(team.won / (team.won + team.lost)).toFixed(3)}</td>
+            <td>${team.points.for}</td>
+            <td>${team.points.against}</td>
+            <td>${team.points.difference}</td>
+            <td>${team.records.home}${team.records.home.split('-').length === 2 ? '-0' : ''}</td>
+            <td>${team.records.road}${team.records.road.split('-').length === 2 ? '-0' : ''}</td>
+            <td>${team.records.division}${team.records.division.split('-').length === 2 ? '-0' : ''}</td>
+            <td>${team.records.conference}${team.records.conference.split('-').length === 2 ? '-0' : ''}</td>
+            <td>${team.streak}</td>
             `;
             tbody.appendChild(tr);
         })
@@ -61,12 +109,13 @@ export default function FootballStandings() {
             <table id="standings-table-nfl-american">
                 <thead>
                 <tr>
+                    <th>Team</th>
                     <th>W</th>
                     <th>L</th>
                     <th>Pct</th>
                     <th>PF</th>
                     <th>PA</th>
-                    <th>Net Pts</th>
+                    <th>Net</th>
                     <th>Home</th>
                     <th>Road</th>
                     <th>Division</th>
@@ -78,26 +127,29 @@ export default function FootballStandings() {
                 </tbody>
             </table>
         </div>
-        {/* <div className="national-container">
-            <h1 className="nationalstandings">East Conference Standings <img className="easticon" src="https://sportsfly.cbsistatic.com/fly-0425/bundles/sportsmediacss/images/conference-logos/nba/EAST.svg" alt=""></img></h1>
-            <table id="standings-table-nba-e">
+        <div className="national-container">
+            <h1 className="nationalstandings">National Football Conference Standings <img className="nationalicon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/National_Football_Conference_logo.svg/1200px-National_Football_Conference_logo.svg.png" alt=""></img></h1>
+            <table id="standings-table-nfl-national">
                 <thead>
                 <tr>
                     <th>Team</th>
                     <th>W</th>
                     <th>L</th>
                     <th>Pct</th>
-                    <th>GB</th>
+                    <th>PF</th>
+                    <th>PA</th>
+                    <th>Net</th>
                     <th>Home</th>
-                    <th>Away</th>
-                    <th>L10</th>
+                    <th>Road</th>
+                    <th>Division</th>
+                    <th>Conf</th>
                     <th>Strk</th>
                 </tr>
                 </thead>
                 <tbody>
                 </tbody>
             </table>
-        </div> */}
+        </div>
         </div>
     )
 }
