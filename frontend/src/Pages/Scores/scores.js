9c759b68531bcb22d0078473e7606240b7c51ae0
Antonio Cosio <coshio@g.ucla.edu>
2023-03-15 20:26:31 -0700

    Added NFL Logo to Scores home page
    Centered the navbar to center with the rest of the icons

--- frontend/src/Pages/Scores/scores.js
+++ frontend/src/Pages/Scores/scores.js
@@ -34,12 +34,15 @@ function Scores () {
             <img className="premleague-title-icon" src="https://media.api-sports.io/football/leagues/39.png" alt=""/>
          </h1>
          <h1>
-            <img className="bundesliga-title-icon" src="https://media.api-sports.io/football/leagues/78.png" alt=""/>
-         </h1>
-         <h1>
             <img className="laliga-title-icon" src="https://media.api-sports.io/football/leagues/140.png" alt=""/>
          </h1>
+         <h1>
+            <img className="bundesliga-title-icon" src="https://media.api-sports.io/football/leagues/78.png" alt=""/>
+         </h1>
       </div>
+      <h1> 
+         <img className="nfl-title-icon" src="https://cdn.cookielaw.org/logos/46acd508-0e8d-40cd-af22-1a8bdfa6da60/e9c29623-f807-422e-9944-964ce7fff1e0/a67792a1-43d4-44d0-8d5e-99ce69b835d9/National_Football_League_logo.svg.png" alt=""></img>
+      </h1>
    </div>
      
      </>); 
